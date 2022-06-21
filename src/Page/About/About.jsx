45bc5ec4ca19c4fd3e2313dbc19fd88034dce362
Apri <apriantohlintar81@gmail.com>
2022-06-21 11:55:35 +0700

    upload  commit

--- src/Page/About/About.jsx
+++ src/Page/About/About.jsx
@@ -1,61 +1,64 @@
 import React from "react";
 import './About.css';
-// import person from "../../assets/person.png";
-
+import { Helmet } from 'react-helmet';
 
 const About = () =>{
   return(
     <div className="body-content">
-        <div className="about-card">
-          <p className="title-text">about me</p>
-          <br />
-          <p className="desc-text">I am a software engineer with a demonstrated history of working in the computer software industry and I have proven experience in developing web-based application for procurement system. I skilled using Ruby on Rails, React JS, CSS and HTML to develop web-based application</p>
-          <br />
-          <br />
-          <p className="title-text">experience</p>
-          <br />
+      <Helmet>
+        <title>About</title>
+      </Helmet>
+        
+      <div className="about-card">
+        <p className="title-text">about me</p>
+        <br />
+        <p className="desc-text">I am a software engineer with a demonstrated history of working in the computer software industry and I have proven experience in developing web-based application for procurement system. I skilled using Ruby on Rails, React JS, CSS and HTML to develop web-based application</p>
+        <br />
+        <br />
+        <p className="title-text">experience</p>
+        <br />
 
-        </div>
-        <div className="experience-card">
+      </div>
+      <div className="experience-card">
 
+          <div className="column">
+              <div className="card">
+                <div className="circle-container-left"></div>
+                <p className="experience-text">Lorem Ipsum is simply dummy 
+                  text of the printing and
+                  typesetting industry. 
+                  Lorem Ipsum has been the 
+                  industry's standard dummy 
+                  text ever since the 1500s</p>
+              </div>
+            </div>
+          
             <div className="column">
-                <div className="card">
-                  <div className="circle-container-left"></div>
-                  <p className="experience-text">Lorem Ipsum is simply dummy 
-                    text of the printing and
-                    typesetting industry. 
-                    Lorem Ipsum has been the 
-                    industry's standard dummy 
-                    text ever since the 1500s</p>
-                </div>
+              <div className="card">
+                  <div className="circle-container-center"></div>
+                <p className="experience-text">Lorem Ipsum is simply dummy 
+                  text of the printing and
+                  typesetting industry. 
+                  Lorem Ipsum has been the 
+                  industry's standard dummy 
+                  text ever since the 1500s</p>
               </div>
+            </div>
             
-              <div className="column">
-                <div className="card">
-                    <div className="circle-container-center"></div>
-                  <p className="experience-text">Lorem Ipsum is simply dummy 
-                    text of the printing and
-                    typesetting industry. 
-                    Lorem Ipsum has been the 
-                    industry's standard dummy 
-                    text ever since the 1500s</p>
-                </div>
+            <div className="column">
+              <div className="card">
+                  <div className="circle-container-right"> </div>
+                <p className="experience-text">Lorem Ipsum is simply dummy 
+                  text of the printing and
+                  typesetting industry. 
+                  Lorem Ipsum has been the 
+                  industry's standard dummy 
+                  text ever since the 1500s</p>
               </div>
-              
-              <div className="column">
-                <div className="card">
-                    <div className="circle-container-right"> </div>
-                  <p className="experience-text">Lorem Ipsum is simply dummy 
-                    text of the printing and
-                    typesetting industry. 
-                    Lorem Ipsum has been the 
-                    industry's standard dummy 
-                    text ever since the 1500s</p>
-                </div>
-              </div>
+            </div>
 
 
-        </div>
+      </div>
 
     </div>
     
--- src/Page/Homepage/Homepage.jsx
+++ src/Page/Homepage/Homepage.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { Helmet } from 'react-helmet';
 import './Homepage.css';
 import person from "../../assets/person.png";
+import file_cv from "../../assets/CV-Aprianto-Halilintar.pdf";
 
 
 
 const Homepage = () =>{
   return(    
     <div className="body-home">
+	  <Helmet>
+        <title>Home</title>
+      </Helmet>
       <div className="detail">
 		    <p className="title-text">introduction</p>
 		    <h1>Hello! I ‘m Apri. I am a <span>Software Engineer</span></h1>
 		    <p className="desc-text">This is my official portfolio website to show
 			  <br/> my work experience in web development
 		    </p>
-	    <a href="#">DOWNLOAD CV</a>
+	    <a href={file_cv} download="CV-Aprianto-Halilintar.pdf">DOWNLOAD CV</a>
 	  </div>
 	  <div className="images">
 		  <img src={person} className="person"/>
